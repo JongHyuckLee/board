d965d33e491f7aed93b9a321dcf760ba7d1bd881
jiyoonkim124 <jiyoonkim0124@gmail.com>
2018-04-17 22:41:32 +0900

    ok

--- myFirst.py
+++ myFirst.py
@@ -2,11 +2,25 @@ from flask import Flask, session, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/user')
+def showUserName() :
+    return render_template('user.html', name=session['userName'])
+
+@app.route('/post/<int:postId>')
+def showPost(postId) :
+    return postId
 
 @app.route('/')
-def join_form():
-    return render_template('join_form.html')
+def login_form() :
+    return render_template('login_form.html')
 
+@app.route('/login', methods=['POST'])
+def login() :
+    if request.method== 'POST' :
+        session['userName']=request.form['userName']
+        return redirect(url_for('showUserName'))
+    else :
+        return 'login failed'
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080)
+    if __name__ == "__main__":
+        app.run(host="127.0.0.1", port=8080)
